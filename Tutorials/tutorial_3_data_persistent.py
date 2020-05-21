fab06f5d176d5309e378a5bef3f7e14367e08d0b
DmitriiDenisov <dmitryhse@gmail.com>
2020-05-22 00:46:57 +0400

    Add tutorials for Data Storage Source: https://github.com/python-telegram-bot/python-telegram-bot/wiki/Storing-bot,-user-and-chat-related-data

---
+++ Tutorials/tutorial_3_data_persistent.py
@@ -0,0 +1,48 @@
+from telegram.ext import Updater, CommandHandler, PicklePersistence
+import os, sys
+
+PROJECT_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(PROJECT_PATH)
+f = open(os.path.join(PROJECT_PATH, 'token.txt'), 'r')
+token = f.read(100)
+
+from uuid import uuid4
+from telegram.ext import Updater, CommandHandler
+
+
+def put(update, context):
+    """Usage: /put value"""
+    # Generate ID and seperate value from command
+    key = str(uuid4())
+    value = update.message.text.partition(' ')[2]
+
+    # Store value
+    context.user_data[key] = value
+
+    update.message.reply_text(key)
+
+
+def get(update, context):
+    """Usage: /get uuid"""
+    # Seperate ID from command
+    key = update.message.text.partition(' ')[2]
+
+    # Load value
+    try:
+        value = context.user_data[key]
+        update.message.reply_text(value)
+
+    except KeyError:
+        update.message.reply_text('Not found')
+
+
+if __name__ == '__main__':
+    my_persistence = PicklePersistence(filename='data_storage')
+    updater = Updater(token, persistence=my_persistence, use_context=True)
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler('put', put))
+    dp.add_handler(CommandHandler('get', get))
+
+    updater.start_polling()
+    updater.idle()
